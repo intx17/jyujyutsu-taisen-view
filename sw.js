8006e0dfa995fea5fbc9b5f47f179d29185483b2
intx17 <intx17@users.noreply.github.com>
2020-11-22 18:22:55 +0000

    deploy: 44d99ee1b52b6ddf477e8fc9d66426184ebfd837

--- sw.js
+++ sw.js
@@ -2,16 +2,12 @@ importScripts('/my-doomsday-clock-application/_nuxt/workbox.4c4f5ca6.js')
 
 workbox.precaching.precacheAndRoute([
   {
-    "url": "/my-doomsday-clock-application/_nuxt/0b70b0c.js",
-    "revision": "dbc90868fa280b5845700e76c78de80a"
+    "url": "/my-doomsday-clock-application/_nuxt/05e7891.js",
+    "revision": "b96a4059fcd1ca60ec08d0a540177816"
   },
   {
     "url": "/my-doomsday-clock-application/_nuxt/1417b41.js",
     "revision": "13e0b13b50383776d1169c66355f90a2"
-  },
-  {
-    "url": "/my-doomsday-clock-application/_nuxt/221a903.js",
-    "revision": "589226713513dd3109be981accffbf22"
   },
   {
     "url": "/my-doomsday-clock-application/_nuxt/5ce5296.js",
@@ -20,6 +16,10 @@ workbox.precaching.precacheAndRoute([
   {
     "url": "/my-doomsday-clock-application/_nuxt/87a8737.js",
     "revision": "3ed79099c1a18ef64a8e1afd8ec187f9"
+  },
+  {
+    "url": "/my-doomsday-clock-application/_nuxt/8958dc3.js",
+    "revision": "a2aca7330fcf323e422950719f77d662"
   }
 ], {
   "cacheId": "my-doomsday-clock-application",
