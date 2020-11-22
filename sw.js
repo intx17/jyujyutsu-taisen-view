13cf314324edc0f97cd046de37d9cfc5acce8d3f
intx17 <intx17@users.noreply.github.com>
2020-11-22 17:57:24 +0000

    deploy: 0d36ef292590faf9f4612e4a2ce3882f77db26a8

--- sw.js
+++ sw.js
@@ -6,8 +6,8 @@ workbox.precaching.precacheAndRoute([
     "revision": "13e0b13b50383776d1169c66355f90a2"
   },
   {
-    "url": "/my-doomsday-clock-application/_nuxt/33b9764.js",
-    "revision": "10228269fb244b4c3242f125f23d8442"
+    "url": "/my-doomsday-clock-application/_nuxt/7ab6d00.js",
+    "revision": "d9100ebb65e0f8f127be1d99413fed31"
   },
   {
     "url": "/my-doomsday-clock-application/_nuxt/87a8737.js",
